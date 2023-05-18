a4c35273e4c74a6d1c9e9a8d2dd917ce1b389764
ypx <ypx@qq.com>
2023-05-18 21:08:49 +0800

    first commit

---
+++ src/main/java/com/java/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.java.git;
+
+/**
+ * @description:
+ * @author: PelXnEr
+ * @date: 2023/5/18 21:03
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
